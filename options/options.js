59b401b246ae7b000e4c35c47806063523bf7809
Crowdbotics <team@crowdbotics.com>
2023-05-16 22:47:31 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "morning-shadow-40751",
+  url: "https://morning-shadow-40751.botics.co",
+  api: "https://morning-shadow-40751.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
